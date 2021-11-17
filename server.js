964c41a431f322e7531644747f9502e4e760cf36
omar khalif <73026620+Cumar-khalif@users.noreply.github.com>
2021-11-17 08:21:02 -0600

    added all the necessary files-routes,helpers and server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const path = require('path');
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Public folder for clients
+app.use(express.static('public'));
+
+// API route
+require('./routes/apiRoutes')(app);
+// .Develop/routes/apiRoutes
+
+// HTML route
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} `)
+);
